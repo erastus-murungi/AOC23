d3ce406d2955cc77cc6043dd9cf5039b3d3dfd4e
Erastus Murungi <erastuswagwan@gmail.com>
2023-12-04 11:30:12 -0800

    day 4 part 2

--- test_aoc.py
+++ test_aoc.py
@@ -4,34 +4,27 @@ from day1 import (
 )
 from day2 import Game, validate, validate2
 from day3 import sum_nums_adjacent_to_symbol, compute_sum_gear_ratios
+from day4 import compute_total_card_value, compute_num_scratchcards_won
 
 
 def test_day1_part1():
-    with open("input/day1.txt") as f:
-        _calibration_document = f.read()
-    assert compute_sum_of_all_calibration_values(_calibration_document) == 54708
+    with open("input/day1_tiny.txt") as f:
+        calibration_document = f.read()
+    assert compute_sum_of_all_calibration_values(calibration_document) == 142
 
-    assert (
-        compute_sum_of_all_calibration_values_str_nums(
-            """1abc2
-               pqr3stu8vwx
-               a1b2c3d4e5f
-               treb7uchet"""
-        )
-        == 142
-    )
+    with open("input/day1.txt") as f:
+        calibration_document = f.read()
+    assert compute_sum_of_all_calibration_values(calibration_document) == 54708
 
 
 def test_day1_part2():
     with open("input/day1_tiny.txt") as f:
-        _calibration_document = f.read()
-    assert compute_sum_of_all_calibration_values_str_nums(_calibration_document) == 281
+        calibration_document = f.read()
+    assert compute_sum_of_all_calibration_values_str_nums(calibration_document) == 281
 
     with open("input/day1.txt") as f:
-        _calibration_document = f.read()
-    assert (
-        compute_sum_of_all_calibration_values_str_nums(_calibration_document) == 54087
-    )
+        calibration_document = f.read()
+    assert compute_sum_of_all_calibration_values_str_nums(calibration_document) == 54087
 
 
 def test_day2_part1():
@@ -57,6 +50,7 @@ def test_day2_part2():
 def test_day3_part1():
     with open("input/day3_tiny.txt") as f:
         assert sum_nums_adjacent_to_symbol(f.read()) == 4361
+
     with open("input/day3.txt") as f:
         assert sum_nums_adjacent_to_symbol(f.read()) == 538046
 
@@ -64,5 +58,26 @@ def test_day3_part1():
 def test_day3_part2():
     with open("input/day3_tiny.txt") as f:
         assert compute_sum_gear_ratios(f.read()) == 467835
+
     with open("input/day3.txt") as f:
         assert compute_sum_gear_ratios(f.read()) == 81709807
+
+
+def test_day4_part1():
+    with open("input/day4_tiny.txt") as f:
+        cards = f.read()
+    assert compute_total_card_value(cards) == 13
+
+    with open("input/day4.txt") as f:
+        cards = f.read()
+    assert compute_total_card_value(cards) == 24160
+
+
+def test_day4_part2():
+    with open("input/day4_tiny.txt") as f:
+        cards = f.read()
+    assert compute_num_scratchcards_won(cards) == 30
+
+    with open("input/day4.txt") as f:
+        cards = f.read()
+    assert compute_num_scratchcards_won(cards) == 5659035
