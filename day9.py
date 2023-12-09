9fdac4c2d632aa85626b8244e48098d8026d60a4
Erastus Murungi <erastuswagwan@gmail.com>
2023-12-09 13:54:13 -0800

    refactor

--- day9.py
+++ day9.py
@@ -1,38 +1,30 @@
 from utils import AOCChallenge, extract_numbers
 
 
-def recurse_part1(history: list[int] | tuple[int]) -> int:
+def recurse(history: list[int] | tuple[int], reverse: bool = False) -> int:
     if all(num == 0 for num in history):
         return 0
     else:
-        val = recurse_part1([a - b for a, b in zip(history[1:], history)])
-        return history[-1] + val
+        val = recurse(
+            [b - a if reverse else a - b for a, b in zip(history[1:], history)], reverse
+        )
+        return history[0 if reverse else -1] + val
 
 
-def recurse_part2(history: list[int] | tuple[int]) -> int:
-    if all(num == 0 for num in history):
-        return 0
-    else:
-        val = recurse_part2([b - a for a, b in zip(history[1:], history)])
-        return history[0] + val
+def process_data(filename: str, reverse: bool = False) -> int:
+    with open(filename) as f:
+        data = f.read().strip()
+        lines = list(map(extract_numbers, data.splitlines()))
+        return sum(map(lambda x: recurse(x, reverse), lines))
 
 
 def part1(filename: str) -> int:
-    with open(filename) as f:
-        data = f.read().strip()
-        lines = list(map(extract_numbers, data.splitlines()))
-        return sum(map(recurse_part1, lines))
+    return process_data(filename)
 
 
 def part2(filename: str) -> int:
-    with open(filename) as f:
-        data = f.read().strip()
-        lines = list(map(extract_numbers, data.splitlines()))
-        return sum(map(recurse_part2, lines))
+    return process_data(filename, reverse=True)
 
 
 day9 = AOCChallenge(9, part1, part2)
 __all__ = [day9]
-
-if __name__ == "__main__":
-    print(part1("input/day9.txt"))
